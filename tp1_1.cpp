4a9fb2f612db9368aed9adb4632655df5c517bd6
thomas mafut <thomasmafut@gmail.com>
2024-03-16 04:31:30 -0300

    punto 3 - apartado e, f

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+void procedimiento() {
+    printf("Punto 3\n\n");
+    int variable, *puntVariable;
+    puntVariable = &variable;
+
+    variable = 123;
+    printf("1). %d\n", *puntVariable);
+    printf("2). %p\n", puntVariable);
+    printf("3). %p\n", &variable);
+    printf("4). %p\n", &puntVariable);
+    printf("5). %d\n", sizeof(variable));
+}
+
 int main() {
-    printf("Hola mundo");
+    printf("Hola mundo\n\n");
+    procedimiento();
     return 0;
 }
